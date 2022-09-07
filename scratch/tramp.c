f8304f37bc7dbe0d572e0ab67c309144d84b3546
tirthj <tirthj@localhost>
2022-09-07 12:52:04 -0600

    trampoline working

--- scratch/tramp.c
+++ scratch/tramp.c
@@ -15,26 +15,13 @@ int main() {
     RT_init();
     MPK_init();
 
-    void* domain_addr = mmap(NULL, 1024*1024, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    if (domain_addr == MAP_FAILED) {
-        perror("mmap");
-        exit(1);
-    }
-    printf("domain_addr: %p\n", domain_addr);
-    ret = pkey_mprotect(domain_addr, 1024*1024, PROT_READ | PROT_WRITE, 1);
-    
-    if (ret < 0) {
-        perror("pkey_mprotect");
-        exit(1);
-    }
-
     struct elf_domain* dom_a = load_elf_domain("add.so", 1);
-
-    void* add_ptr = find_symbol(dom_a, "_add");
-
-    int (*add_fn)(int,int);
-    add_fn = add_ptr;
-    printf("%d", add_fn(34, 35));
+    
+    void* add_ptr = find_symbol(dom_a, "add");
+    int (*add_fn)(int,int) = add_ptr;
+    printf("add_ptr: %p\n", add_ptr);
+    
+    printf("add_res: %d\n", add_fn(34, 35));
 
     return 0;
 }
--- src/main.c
+++ src/main.c
@@ -1,96 +1,93 @@
+#define _GNU_SOURCE
+
+#include <errno.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-#include <errno.h>
-
-
-#include <libavformat/avformat.h>
-#include <libavcodec/avcodec.h>
 
 struct buffer_data {
-    uint8_t *ptr;
-    size_t size;
+  uint8_t *ptr;
+  size_t size;
 };
 
-long long fsize(char* path) {
-    struct stat sb;
+long long fsize(char *path) {
+  struct stat sb;
 
-    if (stat(path, &sb) < 0) {
-        perror("stat");
-        return -1;
-    }
-    printf("size: %lld", sb.st_size);
+  if (stat(path, &sb) < 0) {
+    perror("stat");
+    return -1;
+  }
+  printf("size: %ld", sb.st_size);
 
-    return sb.st_size;
+  return sb.st_size;
 }
 
-static int read_packet(void *opaque, uint8_t *buf, int buf_size)
-{
-    struct buffer_data *bd = (struct buffer_data *)opaque;
-    buf_size = FFMIN(buf_size, bd->size);
-    /* copy internal buffer data to buf */
-    memcpy(buf, bd->ptr, buf_size);
-    bd->ptr  += buf_size;
-    bd->size -= buf_size;
-    return buf_size;
+static int read_packet(void *opaque, uint8_t *buf, int buf_size) {
+  struct buffer_data *bd = (struct buffer_data *)opaque;
+  buf_size = FFMIN(buf_size, bd->size);
+  /* copy internal buffer data to buf */
+  memcpy(buf, bd->ptr, buf_size);
+  bd->ptr += buf_size;
+  bd->size -= buf_size;
+  return buf_size;
 }
 
+int main(int argc, char **argv) {
+  int ret;
+  char *filename;
+  FILE *fp;
+  long long filesize;
+  struct buffer_data bd;
+  uint8_t *io_ctx_buffer;
+  size_t io_ctx_buffer_size = 4096;
+  AVIOContext *io_ctx = NULL;
+  AVFormatContext *ifmt_ctx = NULL;
 
-int main(int argc, char** argv) {
-    int ret;
-    char* filename;
-    FILE* fp;
-    long long filesize;
-    struct buffer_data bd;
-    uint8_t* io_ctx_buffer;
-    size_t io_ctx_buffer_size = 4096;
-    AVIOContext *io_ctx = NULL;
-    AVFormatContext *ifmt_ctx = NULL;
+  if (argc < 2) {
+    fprintf(stderr, "usage: %s <filename>", argv[0]);
+    return 1;
+  }
 
-    if (argc < 2) {
-        fprintf(stderr, "usage: %s <filename>", argv[0]);
-        return 1;
-    }
+  filename = argv[1];
+  filesize = fsize(filename);
 
-    filename = argv[1];
-    filesize = fsize(filename);
+  fp = fopen(filename, "rb");
 
-    fp = fopen(filename, "rb");
+  if (fp == NULL) {
+    perror("fopen");
+    return 1;
+  }
 
-    if (fp == NULL) {
-        perror("fopen");
-        return 1;
-    }
+  printf("filesize: %lld\n", filesize);
 
-    printf("filesize: %lld\n", filesize);
+  uint8_t *video_buffer = (uint8_t *)av_malloc(filesize * sizeof(uint8_t *));
+  filesize = fread(video_buffer, 1, filesize, fp);
 
+  bd.ptr = video_buffer;
+  bd.size = filesize;
 
-    uint8_t* video_buffer = (uint8_t*) av_malloc(filesize * sizeof(uint8_t*));
-    filesize = fread(video_buffer, 1, filesize, fp);
+  io_ctx_buffer = av_malloc(io_ctx_buffer_size);
+  io_ctx = avio_alloc_context(io_ctx_buffer, io_ctx_buffer_size, 0, &bd,
+                              read_packet, NULL, NULL);
 
-    bd.ptr = video_buffer;
-    bd.size = filesize;
+  ifmt_ctx = avformat_alloc_context();
+  ifmt_ctx->pb = io_ctx;
 
+  ret = avformat_open_input(&ifmt_ctx, NULL, 0, 0);
+  if (ret < 0) {
+    fprintf(stderr, "failed ot open input");
+    return 1;
+  }
 
-    io_ctx_buffer = av_malloc(io_ctx_buffer_size);
-    io_ctx = avio_alloc_context(io_ctx_buffer, io_ctx_buffer_size, 0, &bd, read_packet, NULL, NULL);
+  // ret = avformat_find_stream_info(ifmt_ctx, NULL);
+  // if (ret < 0) {
+  //     fprintf(stderr, "failed to find stream info from i/p");
+  //     return 1;
+  // }
 
-    ifmt_ctx = avformat_alloc_context();
-    ifmt_ctx->pb = io_ctx;
+  av_dump_format(ifmt_ctx, 0, filename, 0);
 
-    ret = avformat_open_input(&ifmt_ctx, NULL, 0, 0);
-    if (ret < 0) {
-        fprintf(stderr, "failed ot open input");
-        return 1;
-    }
-
-    // ret = avformat_find_stream_info(ifmt_ctx, NULL);
-    // if (ret < 0) {
-    //     fprintf(stderr, "failed to find stream info from i/p");
-    //     return 1;
-    // }
-
-    av_dump_format(ifmt_ctx, 0, filename, 0);
-
-    return 0;
+  return 0;
 }
